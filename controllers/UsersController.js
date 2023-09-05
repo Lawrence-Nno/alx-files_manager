fbec5258d4226d4668abdf44b0a42759917b5a0f
Lawrence Nno <lawrence.nno@gmail.com>
2023-09-05 10:26:49 +0100

    Task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,15 +12,15 @@ class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
 
-    // check for email and password
+    // Checks for email and password
     if (!email) return response.status(400).send({ error: 'Missing email' });
     if (!password) return response.status(400).send({ error: 'Missing password' });
 
-    // check if the email already exists in DB
+    // Checks if the email already exists in DB
     const emailExists = await dbClient.users.findOne({ email });
     if (emailExists) return response.status(400).send({ error: 'Already exist' });
 
-    // Insert new user
+    // Inserts new user
     const sha1Password = sha1(password);
     let result;
     try {
@@ -45,13 +45,13 @@ class UsersController {
   }
 
   /**
-   * Should retrieve the user base on the token used
+   * Retrieve the user base on the token used
    */
   static async getMe(request, response) {
     const token = request.headers['x-token'];
     if (!token) { return response.status(401).json({ error: 'Unauthorized' }); }
 
-    // Retrieve the user based on the token
+    // Retrieves the user based on the token
     const userId = await findUserIdByToken(request);
     if (!userId) return response.status(401).send({ error: 'Unauthorized' });
 
@@ -62,7 +62,7 @@ class UsersController {
     const processedUser = { id: user._id, ...user };
     delete processedUser._id;
     delete processedUser.password;
-    // Return the user object (email and id only)
+    // Returns the user's object (email & id only)
     return response.status(200).send(processedUser);
   }
 }
